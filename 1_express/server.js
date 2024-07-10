c9741e4a3b97a51ca38c0e12a91de39a997e40e5
lucarl07 <junitinho2007@gmail.com>
2024-07-10 07:54:01 -0300

    :bulb: Basic Express concepts implemented

--- 1_express/server.js
+++ 1_express/server.js
@@ -7,24 +7,30 @@ const PORT = 3333;
 // Atribui uma função Express à constante app
 const app = express();
 
+// Rotas:
+/** Tipos de HTTP Requests:
+ * query params - ...:3333/pessoas?nome="Luiz"&idade=17
+ *  ↪  Rotas do tipo GET (filtros e buscas)
+ * route params - ...:3333/pessoas/5929
+ *  ↪  Rotas do tipo GET, PUT, PATCH e DELETE (listar um elemento)
+ * body params - ...:3333/pessoas
+ *  ↪  Rotas do tipo POST (cadastro de informações)
+ */
+
 app.get("/users", (req, res) => {
-  res.status(200).json({ msg: "GET" });
+  res.status(200).json(["Pessoa 1", "Pessoa 2", "Pessoa 3"]);
 });
 
-app.post("/post", (req, res) => {
-  res.status(200).json({ msg: "POST" });
+app.post("/users", (req, res) => {
+  res.status(201).json(["Pessoa 1", "Pessoa 2", "Pessoa 3", "Pessoa 4"]);
 });
 
-app.put("/put", (req, res) => {
-  res.status(200).json({ msg: "PUT" });
+app.put("/users", (req, res) => {
+  res.status(200).json(["Pessoa 1", "Pessoa 2", "Pessoa 3", "Pessoa 10"]);
 });
 
-app.patch("/patch", (req, res) => {
-  res.status(200).json({ msg: "GET" });
-});
-
-app.get("/delete", (req, res) => {
-  res.status(200).json({ msg: "DELETE" });
+app.delete("/users", (req, res) => {
+  res.status(204).json("Task completed succesfully!");
 });
 
 // Atribuir a porta em que o servidor deve ser iniciado e a sua resposta
