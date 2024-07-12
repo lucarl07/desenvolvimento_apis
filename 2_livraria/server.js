118409172b3f6f70561ce27f1551025e50ac3c22
lucarl07 <junitinho2007@gmail.com>
2024-07-12 08:16:26 -0300

    :construction: Building 2 endpoints at 2_livraria

--- 2_livraria/server.js
+++ 2_livraria/server.js
@@ -25,14 +25,61 @@ conn.connect((err) => {
   }
 
   console.clear();
-  console.log("[Conectado ao banco de dados com êxito...]");
+  console.log("[Banco de dados conectado com sucesso.]");
 
   app.listen(PORT, () => {
     console.log(`Bem-vindo à Livraria API! \nServidor on PORT: ${PORT} 🚀 \n`);
   });
 });
 
+// Ver o catálogo de livros
+app.get("/livros", (req, res) => {
+  // Query para o banco de dados:
+  const sql = "SELECT * FROM livros";
+  console.log(`Query --> ${sql}`);
+
+  conn.query(sql, (err, data) => {
+    if (err) {
+      res.status(500).json({ message: "Erro ao buscar os livros." });
+      return console.log(err);
+    }
+
+    const livros = data;
+    // console.log(data);
+    // console.log(typeof data);
+    res.status(200).json(livros);
+  });
+});
+
+// Cadastrar um novo livro na plataforma
+app.post("/livros", (req, res) => {
+  const { titulo, autor, ano_publicacao, genero, preco } = req.body;
+
+  if (!titulo || !autor || !ano_publicacao || !genero || !preco) {
+    res
+      .status(401)
+      .json({ message: "Dados insuficientes para realizar o cadastro." });
+  }
+
+  const checkSql = `SELECT * FROM livros 
+    WHERE titulo = "${titulo}" 
+    AND autor = "${autor}"
+    AND ano_publicacao = "${ano_publicacao}"`;
+
+  conn.query(checkSql, (err, data) => {
+    if (err) {
+      res.status(500).json({ message: "Erro ao buscar os livros." });
+      return console.log(err);
+    }
+
+    if (data.length > 0) {
+      res.status(409).json({ message: "O livro já existe na livraria." });
+      return console.log(err);
+    }
+  });
+});
+
 // Rota 404
 app.use((request, response) => {
-  response.status(404).json({ mensagem: "Rota não encontrada." });
+  response.status(404).json({ message: "Rota não encontrada." });
 });
