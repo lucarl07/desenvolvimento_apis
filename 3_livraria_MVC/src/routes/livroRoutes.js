3448e25c5c16ad3025fd6cb739be4138b8968dea
lucarl07 <junitinho2007@gmail.com>
2024-07-20 08:45:12 -0300

    :construction: 3_livraria_MVC: Add 4 TBD routes at src/routes/livroRoutes.js

--- 3_livraria_MVC/src/routes/livroRoutes.js
+++ 3_livraria_MVC/src/routes/livroRoutes.js
@@ -8,5 +8,9 @@ const router = Router();
 
 router.get("/", getLivros);
 router.post("/", postLivros);
+router.put("/:id");
+router.delete("/:id");
+router.get("/:id");
+router.get("/:nome");
 
 export default router;
