dff067de4499d6bd55a807350017767e78a76777
lucarl07 <junitinho2007@gmail.com>
2024-07-20 08:08:01 -0300

    :sparkles: 3_livraria_MVC: Add a JSON response to the root route

--- 3_livraria_MVC/src/server.js
+++ 3_livraria_MVC/src/server.js
@@ -9,16 +9,19 @@ import conn from "./config/conn.js";
 import "./models/livroModel.js";
 import "./models/funcionarioModel.js";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 const app = express();
-
-app.get("/", (req, res) => {
-  console.log("Olá mundo!");
-});
 
 app.listen(PORT, () => {
   console.clear();
   console.log("Bem vindo à 2_2_livraria_MVC API!");
   console.log(`Server on PORT: ${PORT} 🚀\n`);
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    mensagem: "Olá mundo! Bem vindo à 2_2_livraria_MVC API!",
+    acesso: `http://localhost:${PORT}/`,
+  });
 });
