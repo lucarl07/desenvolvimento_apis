c86c09ec34c741e1825970be50f7db42a3820ec8
lucarl07 <junitinho2007@gmail.com>
2024-07-20 10:04:16 -0300

    :sparkles: 3_livraria_MVC: Add endpoint GET /clientes

--- 3_livraria_MVC/src/controllers/clientesController.js
+++ 3_livraria_MVC/src/controllers/clientesController.js
@@ -1,7 +1,20 @@
 import conn from "../config/conn.js";
 import { v4 as uuidv4 } from "uuid";
 
-export const buscarClientes = (req, res) => {}
+export const buscarClientes = (req, res) => {
+  const sql = /*sql*/ `
+    SELECT * FROM clientes
+  `;
+
+  conn.query(sql, (err, data) => {
+    if (err) {
+      res.status(500).json({ message: "Erro ao buscar clientes." });
+      return console.log(err);
+    }
+
+    res.status(200).json(data);
+  });
+}
 
 export const adicionarClientes = (req, res) => {}
 
