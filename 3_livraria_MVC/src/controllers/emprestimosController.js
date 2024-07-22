4c69e661b4bfe424972be20636747bf0d81cd122
lucarl07 <junitinho2007@gmail.com>
2024-07-22 09:40:24 -0300

    :sparkles: 3_livraria_MVC: Add endpoint GET /emprestimos

--- 3_livraria_MVC/src/controllers/emprestimosController.js
+++ 3_livraria_MVC/src/controllers/emprestimosController.js
@@ -1,6 +1,16 @@
 import conn from "../config/conn.js";
 
-export const buscarEmprestimos = (req, res) => {}
+export const buscarEmprestimos = (req, res) => {
+  const SQL = /*sql*/ `SELECT * FROM emprestimos`
+
+  conn.query(SQL, (err, data) => {
+    if (err) {
+      res.status(500).json({ mensagem: "Erro ao buscar lista de empréstimos." })
+      return console.log(err);
+    }
+    res.status(200).json(data)
+  })
+}
 
 export const criarEmprestimo = (req, res) => {}
 
