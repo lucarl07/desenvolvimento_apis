8b9c07210d779af064c3440300420fafac62d7fe
lucarl07 <junitinho2007@gmail.com>
2024-07-22 08:00:05 -0300

    :recycle: 3_livraria_MVC: Replace the regular connection for a pool

--- 3_livraria_MVC/src/config/conn.js
+++ 3_livraria_MVC/src/config/conn.js
@@ -1,19 +1,21 @@
-import "dotenv/config"
-import mysql from "mysql2"
+import "dotenv/config";
+import mysql from "mysql2";
 
-const conn = mysql.createConnection({
+const conn = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT
-})
+  port: process.env.MYSQL_PORT,
+});
 
-conn.connect((err) => {
-  if (err) {
-    return console.error(err.stack)
-  }
-  console.log("[Banco de dados conectado com sucesso.]")
-})
+/* Unused connection module: */
+// conn.connect((err) => {
+//   if (err) {
+//     return console.error(err.stack);
+//   }
+//   console.log("[Banco de dados conectado com sucesso.]");
+// });
 
 export default conn;
