7386f2371c22477b3176d0c7ef65bef59f90357a
lucarl07 <junitinho2007@gmail.com>
2024-07-24 08:23:01 -0300

    ⚙️ 4_livraria: Set connection at src/config/conn.js

---
+++ 4_bustrack/src/config/conn.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import mysql from "mysql2";
+
+const conn = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
+});
+
+try {
+  console.log("[API] MySQL conectado...")
+} catch (error) {
+  console.log(error)
+}
+
+export default conn;
