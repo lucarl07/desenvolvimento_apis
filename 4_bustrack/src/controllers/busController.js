17278bf474a3ffb0ec0e4ba7618bda95cf1beddf
lucarl07 <junitinho2007@gmail.com>
2024-07-26 08:17:58 -0300

    🚧 4_bustrack: Add the /motoristas controller to its router

---
+++ 4_bustrack/src/controllers/busController.js
@@ -0,0 +1,8 @@
+import conn from "../config/conn.js";
+import { v4 as uuidv4 } from "uuid";
+
+export const adicionarOnibus = (req, res) => {}
+
+export const buscarOnibusPorId = (req, res) => {}
+
+export const buscarTodosOsOnibus = (req, res) => {}
--- 4_bustrack/src/routes/busRouter.js
+++ 4_bustrack/src/routes/busRouter.js
@@ -2,11 +2,16 @@
 import { Router } from "express";
 
 // Controladores:
+import { 
+  adicionarOnibus, 
+  buscarOnibusPorId, 
+  buscarTodosOsOnibus 
+} from "../controllers/busController.js";
 
 const router = Router();
 
-router.post("/")
-router.get("/:id_onibus")
-router.get("/")
+router.post("/", adicionarOnibus)
+router.get("/:id_onibus", buscarOnibusPorId)
+router.get("/", buscarTodosOsOnibus)
 
 export default router;
